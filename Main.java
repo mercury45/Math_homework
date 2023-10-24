529d53f8e383f63c5e48cde3123aee51de60d85a
Zharenov Nikita <lking1231@mail.ru>
2023-10-24 18:37:27 +0300

    Started method 'solve' and ready 4/5 methods in 1st tast

--- Main.java
+++ Main.java
@@ -3,23 +3,28 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String [] args) {
 		Scanner sc = new Scanner(System.in);
-		Matrix matrix = new Matrix();
-		matrix.inputMatrix();
+		// Matrix matrix = new Matrix();
+		// matrix.inputMatrix();
 
-		Matrix anotherMatrix = new Matrix();
-		anotherMatrix.inputMatrix();
-		matrix.printMatrix();
-		System.out.println("-----");
-		anotherMatrix.printMatrix();
-		int f = 0;
+		// Matrix anotherMatrix = new Matrix();
+		// anotherMatrix.inputMatrix();
+		// matrix.printMatrix();
+		// System.out.println("-----");
+		// anotherMatrix.printMatrix();
+		// int f = 0;
 		Matrix newArr = new Matrix();
-		try {
-			newArr = matrix.multiply(anotherMatrix);
-		} catch (Exception e) {
-			System.out.println("NULL, couldn't multiply");
+		newArr = newArr.solve(newArr);
+		
+		// try {
+		// 	newArr = matrix.multiply(anotherMatrix);
+		// } catch (Exception e) {
+		// 	System.out.println("NULL, couldn't multiply");
 
-			f = 1;
-		}
-		newArr.printMatrix();
+		// 	f = 1;
+		// }
+		// newArr.printMatrix();
+
+		// matrix.plus(anotherMatrix);
+		// matrix.printMatrix();
 	}
 }
--- Matrix.java
+++ Matrix.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 import java.util.Locale;
+import java.util.Arrays;
 public class Matrix {
 
-	int[][] array;
+	double[][] array;
 	int rowsCount;
 	int colsCount;
 
@@ -14,7 +15,7 @@ public class Matrix {
 			colsCount = sc.nextInt();
 		} while (rowsCount == 0 || colsCount == 0);
 
-		this.array = new int[rowsCount][colsCount];
+		this.array = new double[rowsCount][colsCount];
 
 		int method = methodAsk();
 		if (method == 1) {
@@ -28,7 +29,7 @@ public class Matrix {
 	}
 	public int methodAsk() {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Input your method num (1/2):");
+		System.out.print("Input your method num (1 - by strings/2 - elem by elem):");
 		int methodNum = sc.nextInt();
 		return methodNum;
 	}
@@ -61,7 +62,7 @@ public class Matrix {
 		if (this.colsCount == newArray.rowsCount) {
 			multipliedMatrix.rowsCount = this.rowsCount;
 			multipliedMatrix.colsCount = newArray.colsCount;
-			multipliedMatrix.array = new int[this.rowsCount][newArray.colsCount];
+			multipliedMatrix.array = new double[this.rowsCount][newArray.colsCount];
 			for (int i = 0; i < this.rowsCount; i++) {
 				for (int j = 0; j < newArray.colsCount; j++) {
 					for (int k = 0; k < this.colsCount; k++) {
@@ -94,9 +95,26 @@ public class Matrix {
 		
 	}
 
+	public Matrix plus(Matrix matrix) {
+		int m1 = this.rowsCount; 
+		int n1 = this.colsCount;
+		int m2 = matrix.rowsCount; 
+		int n2 = matrix.colsCount;
+
+		if (m1 == m2 && n1 == n2) {
+			for (int i = 0; i < m1; i++) {
+				for (int j = 0; j < n1; j++) {
+					this.array[i][j] += matrix.array[i][j];
+				}
+			}
+		}
+		return this;
+	}
+
+
 	public void printMatrix() {
 		if (rowsCount != 0 && colsCount != 0) {
-			System.out.println("Matrix (" + rowsCount + " * " + colsCount + "):");
+			System.out.println("You have matrix (" + rowsCount + " * " + colsCount + "):");
 			System.out.print(toString());
 		} else {
 			System.out.println("Matrix is null");
@@ -113,5 +131,41 @@ public class Matrix {
 			line += "\n";
 		}
 		return line;
+	}
+
+	public Matrix solve(Matrix vector) {
+		System.out.println("doing");
+		double[][] array = new double[this.rowsCount][this.colsCount+1];
+		for (int i = 0; i < this.colsCount; i++) {
+			array[i] = Arrays.copyOf(this.array[i],this.colsCount);
+			array[i][colsCount] = vector.array[i][colsCount-1];
+		}
+
+		// 1 2 3   k = (-1) * a[i][j] * a[i][i];
+		// 3 4 5
+		// 5 5 5
+		double div;
+		for (int i = 0; i < array.length-1; i++) {
+			for (int j = i+1; j < array[0].length; j++) {
+				System.out.println("++");
+				div = (-1) * array[j][i] / array[i][i];
+				array[j][i] = 0;
+				for (int k = 0; k < array.length; k++) {
+					if (i==k) continue;
+					array[j][k] += div*array[i][k];
+
+				}
+			}
+		}
+		
+		for (int i =0;i<array.length;i++){
+			for(int j=0;j<array[0].length;j++){
+				System.out.println(array[i][j] + " ");
+			}
+			System.out.println();
+
+		}
+
+		return vector;
 	}
 }
